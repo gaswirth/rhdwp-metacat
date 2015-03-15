bedab4144da320f0f82e16c6d0f93bb452a04544
Nick Gaswirth <nick@roundhouse-designs.com>
2015-03-15 16:02:56 -0400

    Docblocks added

--- rhd-metacat.php
+++ rhd-metacat.php
@@ -6,34 +6,17 @@
  * Author URI: http://roundhouse-designs.com
  * Version: 0.1
 **/
-
 // Adapted from http://php.quicoto.com/add-metadata-categories-wordpress/
 
-// Sample front-end retrieval:
-/*
-$cats = get_categories();
 
-foreach ( $cats as $cat ) {
-	$catmeta = get_option( "taxonomy_" . $cat->term_id );
-	if ( !$catmeta['excluded'] || $catmeta['excluded'] === 0 ) {
-		echo	'<li class="cat-item">' .
-					'<a href="' . get_category_link( $cat->cat_ID ) . '">' . $cat->name . '</a>' .
-				'</li>';
-	}
-}
-*/
-
-/* ==========================================================================
-	Plugin Setup
-   ========================================================================== */
-
-define( 'RHD_METACAT_DIR', plugin_dir_url(__FILE__) );
-
-
-/* ==========================================================================
-	MetaCat
-   ========================================================================== */
-
+/**
+ * rhd_edit_excluded_category_field function.
+ * Adds field to "edit category" screen
+ *
+ * @access public
+ * @param mixed $term
+ * @return void
+ */
 function rhd_edit_excluded_category_field( $term ) {
 	$term_id = $term->term_id;
 	$term_meta = get_option( "taxonomy_$term_id" ); ?>
@@ -54,6 +37,14 @@ function rhd_edit_excluded_category_field( $term ) {
 add_action( 'category_edit_form_fields', 'rhd_edit_excluded_category_field' );
 
 
+/**
+ * rhd_create_excluded_category_field function.
+ * Adds field to "create category" screen
+ *
+ * @access public
+ * @param mixed $term
+ * @return void
+ */
 function rhd_create_excluded_category_field( $term ) {
 	$term_id = $term->term_id;
 	$term_meta = get_option( "taxonomy_$term_id" ); ?>
@@ -74,6 +65,14 @@ function rhd_create_excluded_category_field( $term ) {
 add_action( 'category_add_form_fields', 'rhd_create_excluded_category_field' );
 
 
+/**
+ * rhd_save_tax_meta function.
+ * Saves metadata
+ *
+ * @access public
+ * @param mixed $term_id
+ * @return void
+ */
 function rhd_save_tax_meta( $term_id ) {
 	if ( isset( $_POST['term_meta'] ) ) {
 		$term_meta = array();
@@ -88,14 +87,32 @@ add_action( 'edited_category', 'rhd_save_tax_meta', 10, 2 );
 add_action( 'create_category', 'rhd_save_tax_meta', 10, 2 );
 
 
-// Add column to Category list
+
+/**
+ * rhd_excluded_category_columns function.
+ * Add column to Category list
+ *
+ * @access public
+ * @param mixed $columns
+ * @return void
+ */
 function rhd_excluded_category_columns( $columns ) {
 	return array_merge( $columns, array( 'excluded' =>  __( 'Excluded from Dropdown' ) ) );
 }
 add_filter( 'manage_edit-category_columns' , 'rhd_excluded_category_columns' );
 
 
-// Add the value to the column
+
+/**
+ * rhd_excluded_category_columns_values function.
+ * Add the value to the column
+ *
+ * @access public
+ * @param mixed $deprecated
+ * @param mixed $column_name
+ * @param mixed $term_id
+ * @return void
+ */
 function rhd_excluded_category_columns_values( $deprecated, $column_name, $term_id) {
 	if ( $column_name === 'excluded' ) {
 		$term_meta = get_option( "taxonomy_$term_id" );
@@ -108,3 +125,25 @@ function rhd_excluded_category_columns_values( $deprecated, $column_name, $term_
 	}
 }
 add_action( 'manage_category_custom_column' , 'rhd_excluded_category_columns_values', 10, 3 );
+
+
+
+/**
+ * rhd_list_included_cats function.
+ * Front-end retrieval
+ *
+ * @access public
+ * @return void
+ */
+function rhd_list_included_cats() {
+	$cats = get_categories();
+
+	foreach ( $cats as $cat ) {
+		$catmeta = get_option( "taxonomy_" . $cat->term_id );
+		if ( !$catmeta['excluded'] || $catmeta['excluded'] === 0 ) {
+			echo	'<li class="cat-item">' .
+						'<a href="' . get_category_link( $cat->cat_ID ) . '">' . $cat->name . '</a>' .
+					'</li>';
+		}
+	}
+}
